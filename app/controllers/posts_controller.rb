3090720b003e9d359f1dcc529bcbd6137fbead2d
RupertJohn <rupertsaxton@gmail.com>
2019-03-23 21:14:15 -0400

    Provide post creation and viewing
    There are no links on the pages yet. This will come in a navbar soon

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,15 +2,47 @@ class PostsController < ApplicationController
   before_action :authorize, only: [:new, :create, :edit, :update, :destroy]
 
   def all
-
+    @posts = Post.all.by_date_created.limit(2)
   end
 
   def index
-    @posts = Post.all.sort_by { |post| post.created_at }
+    @posts = Post.all.by_date_created
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params.merge({user_id: current_user.id}))
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    Post.destroy(params[:id])
+    redirect_to posts_path
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  scope :by_date_created, -> { order('created_at desc') }
 end
