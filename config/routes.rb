bd964544a5d1eafd25c2f16d5fb89edfc1e4a918
RupertJohn <rupertsaxton@gmail.com>
2019-03-24 16:27:05 -0400

    Add about section

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#all'
 
+  get 'about', to: 'static#about', as: 'about'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
